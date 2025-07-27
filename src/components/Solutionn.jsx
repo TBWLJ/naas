11f7a7a3da7a6e876aaf4fd0dd73a6887af53915
TBWLJ <taiwoayomide202@gmail.com>
2025-07-27 18:46:18 +0100

    update

--- src/components/Solutionn.jsx
+++ src/components/Solutionn.jsx
@@ -33,7 +33,7 @@ const Solutionn = () => {
       <>
         <div className="text-center mt-28" data-aos="fade-up" data-aos-easing="linear"
         data-aos-duration="1500">
-          <h1 className="text-3xl font-bold text-green-500">Our Features</h1>
+          <h1 className="text-3xl font-bold text-green-500">About us</h1>
         </div>
         <section className="flex flex-wrap justify-evenly px-5 pb-10" data-aos="fade-up" data-aos-easing="linear"
         data-aos-duration="1500">
